bf691967d16b01f2d929f532acf89e90c8de1f8c
Max Aubain <max.aubain@gmail.com>
2019-07-10 10:36:36 +0200

    Added update to 'seeds' and 'index' files to include animal attribute.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-MenuItem.create(name: 'Kitty Heaven', description: 'Fresh, seared yellow tail tuna, served on a bed of catnip picked daily from our garden.', price: 140)
+MenuItem.create(name: 'Kitty Heaven', description: 'Fresh, seared yellow tail tuna, served on a bed of catnip picked daily from our garden.', price: 140, animal: 'cat')
