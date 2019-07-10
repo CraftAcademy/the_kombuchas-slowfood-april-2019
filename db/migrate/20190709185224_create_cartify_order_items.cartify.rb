33111d2d2ff7b0d29b64eb5f34a3e9b90e95de87
Seth NeJame <sethnejame@gmail.com>
2019-07-10 14:46:11 +0200

    Small bit of styling to differentiate order items

--- db/migrate/20190709185224_create_cartify_order_items.cartify.rb
+++ db/migrate/20190709185224_create_cartify_order_items.cartify.rb
@@ -6,6 +6,10 @@ class CreateCartifyOrderItems < ActiveRecord::Migration[5.2]
       t.decimal :unit_price, precision: 8, scale: 2
       t.decimal :total_price, precision: 8, scale: 2
       t.integer "#{Cartify.product_class.to_s.downcase}_id".to_sym, foreign_key: true
+      
+      # the following line creates an integer column in the database
+      # called 'order_id', an index for it, and a foreign key constraint
+      # that points to the 'id' column of the 'cartify_orders' table
       t.references :order,
                     foreign_key: { to_table: :cartify_orders }
 
